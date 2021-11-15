39fd0bded7d664077bb255b46aad0b37b9f20d97
Montana Montana <mnt_13@mail.ru>
2021-11-15 18:00:53 +0300

    lesson4

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+export const Home = () => {
+    
+    return (
+        <>
+           <div>
+               <h1>Hello my friend!</h1>
+           </div>
+        </>
+    )
+
+}
+
+export default Home;
